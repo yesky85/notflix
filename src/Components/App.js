44e980e416ba87412c1e04c5caab07d67abb8a82
kec <57247368+yesky85@users.noreply.github.com>
2020-04-05 23:22:36 +0900

    React Router Part One

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Router from "Components/Router";
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Router />
+      </>
+    );
+  }
+}
+
+export default App;
