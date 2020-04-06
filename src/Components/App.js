64ee1ae7a395d8b853d9206c2dae9dca5c66d5f8
kec <57247368+yesky85@users.noreply.github.com>
2020-04-06 20:55:07 +0900

    GlobalStyles and Header

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Router from "Components/Router";
+import GlobalStyles from "Components/GlobalStyles";
 
 class App extends React.Component {
   render() {
     return (
       <>
         <Router />
+        <GlobalStyles />
       </>
     );
   }
