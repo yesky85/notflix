dcd2744484d901d9096278a4c290d95485901215
kec <57247368+yesky85@users.noreply.github.com>
2020-04-08 10:45:36 +0900

    Search Container

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -11,6 +11,7 @@ import Header from "Components/Header";
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
+import Detail from "Routes/Detail";
 
 export default () => (
   <Router>
@@ -19,6 +20,8 @@ export default () => (
       <Route path="/" exact component={Home} />
       <Route path="/tv" exact component={TV} />
       <Route path="/search" exact component={Search} />
+      <Route path="/movie/:id" exact component={Detail} />
+      <Route path="/tv/:id" exact component={Detail} />
       <Redirect from="*" to="/" />
     </Switch>
   </Router>
--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SearchPresenter from "./SearchPresenter";
+import { moviesApi, tvApi } from "api";
 
 export default class extends React.Component {
   state = {
@@ -9,6 +10,35 @@ export default class extends React.Component {
     error: null,
     loading: false,
   };
+
+  handleSubmit = () => {
+    const { searchTerm } = this.state;
+    if (searchTerm !== "") {
+      this.searchByTerm();
+    }
+  };
+
+  searchByTerm = async () => {
+    const { searchTerm } = this.state;
+    this.setState({ loading: true });
+    try {
+      const {
+        data: { results: movieResults },
+      } = await moviesApi.search(searchTerm);
+      const {
+        data: { results: tvResults },
+      } = await tvApi.search(searchTerm);
+      this.setState({
+        movieResults,
+        tvResults,
+      });
+    } catch {
+      this.setState({ error: "Can't find results." });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
   render() {
     const { movieResults, tvResults, searchTerm, error, loading } = this.state;
     return (
@@ -18,6 +48,7 @@ export default class extends React.Component {
         searchTerm={searchTerm}
         error={error}
         loading={loading}
+        handleSubmit={this.handleSubmit}
       />
     );
   }
