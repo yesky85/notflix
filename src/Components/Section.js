895d2d82ef1a29f02426329d6ed197eabae96654
kec <57247368+yesky85@users.noreply.github.com>
2020-04-08 23:30:39 +0900

    TVPresenter and Loader Components

--- src/Components/Section.js
+++ src/Components/Section.js
@@ -15,6 +15,9 @@ const Title = styled.span`
 
 const Grid = styled.div`
   margin-top: 25px;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 125px);
+  gap: 25px;
 `;
 
 const Section = ({ title, children }) => (
