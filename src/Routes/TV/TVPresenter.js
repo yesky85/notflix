f73c0cfce18f94ffd030f2aa75ba25c8075f2ba6
kec <57247368+yesky85@users.noreply.github.com>
2020-04-09 22:26:06 +0900

    Rendering Poster Component

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -5,6 +5,7 @@ import styled from "styled-components";
 import Section from "Components/Section";
 import Loader from "Components/Loader";
 import Message from "Components/Message";
+import Poster from "Components/Poster";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -18,21 +19,42 @@ const TVPresenter = ({ topRated, popular, airingToday, error, loading }) =>
       {topRated && topRated.length > 0 && (
         <Section title="Top Rated Show">
           {topRated.map((show) => (
-            <span key={show.id}>{show.name}</span>
+            <Poster
+              key={show.id}
+              id={show.id}
+              imageUrl={show.poster_path}
+              title={show.original_name}
+              rating={show.vote_average}
+              year={show.first_air_date.substring(0, 4)}
+            />
           ))}
         </Section>
       )}
       {popular && popular.length > 0 && (
         <Section title="Popular Show">
           {popular.map((show) => (
-            <span key={show.id}>{show.name}</span>
+            <Poster
+              key={show.id}
+              id={show.id}
+              imageUrl={show.poster_path}
+              title={show.original_name}
+              rating={show.vote_average}
+              year={show.first_air_date.substring(0, 4)}
+            />
           ))}
         </Section>
       )}
       {airingToday && airingToday.length > 0 && (
         <Section title="Airing Today">
           {airingToday.map((show) => (
-            <span key={show.id}>{show.name}</span>
+            <Poster
+              key={show.id}
+              id={show.id}
+              imageUrl={show.poster_path}
+              title={show.original_name}
+              rating={show.vote_average}
+              year={show.first_air_date.substring(0, 4)}
+            />
           ))}
         </Section>
       )}
