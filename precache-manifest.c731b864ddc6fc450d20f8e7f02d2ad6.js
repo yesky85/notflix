41a1d7e8bf57a05922090ca7fd2a3a25ec9b18cd
kec <57247368+yesky85@users.noreply.github.com>
2020-04-14 12:51:35 +0900

    Updates

---
+++ precache-manifest.c731b864ddc6fc450d20f8e7f02d2ad6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f4ce76e6f9c0dda010ef267146baaff",
+    "url": "/notflix/index.html"
+  },
+  {
+    "revision": "3b3074adec141568a790",
+    "url": "/notflix/static/js/2.9ae85902.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/notflix/static/js/2.9ae85902.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a74d8fd9dd7d5a6ba1b",
+    "url": "/notflix/static/js/main.c5799360.chunk.js"
+  },
+  {
+    "revision": "66731c70f415d2283802",
+    "url": "/notflix/static/js/runtime-main.f6a22332.js"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/notflix/static/media/noPosterSmall.87a0eb6a.png"
+  }
+]);
